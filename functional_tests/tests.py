0ab5088a0b693004e3d3c2df4568d93b58f0ca51
kappa5400 <kappa6708@gmail.com>
2025-07-04 11:07:35 -0500

    test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,7 @@ MAX_WAIT = 5
 
 class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
